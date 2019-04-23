6b2f345a3153914ef465df172a5df22fccf64a53
Alexey Maltsev <aamaltsev@skbkontur.ru>
2019-04-23 23:56:40 +0500

    Completed TwoSum

---
+++ TwoSum/main.go
@@ -0,0 +1,21 @@
+package main
+
+func main() {
+
+}
+
+func twoSum(nums []int, target int) []int {
+	compliments := make(map[int]int)
+	for i, n := range nums {
+		compliments[n] = i
+	}
+
+	for i, n := range nums {
+		compliment := target - n
+		compIdx, ok := compliments[compliment]
+		if ok && i != compIdx {
+			return []int{i, compIdx}
+		}
+	}
+	return nil
+}
---
+++ TwoSum/main_test.go
@@ -0,0 +1,10 @@
+package main
+
+import "testing"
+
+func TestTwoSum(t *testing.T) {
+	comp := twoSum([]int{3, 2, 4}, 6)
+	if comp[0] != 1 || comp[1] != 2 {
+		t.Error("Failed to get expected compliments")
+	}
+}
